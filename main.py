98d9ec5df14abb70a674e752045558deaa497a69
urvitank27 <97378726+urvitank27@users.noreply.github.com>
2022-10-06 17:23:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-import uvicorn, search_query_evaluator, gaussianNB
-#import test_scraping
+import uvicorn, gaussianNB
 
 app = FastAPI()
 
@@ -18,22 +17,10 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
         content=jsonable_encoder({"detail": exc.errors(), "Error": "Something went wrong"}),
 )
 
-@app.post("/search")
-@app.get("/search")
-async def search(query : str) :
-    return search_query_evaluator.search_results(query)
-
 @app.post("/predict")
 @app.get("/predict")
 async def predict(query : str) :
     return gaussianNB.predictDisease(query)
-
-"""
-@app.post("/find")
-@app.get("/find")
-async def find(lat : str, lon : str, search_type : str) :
-    return test_scraping.find_nearest(lat, lon, search_type)
-"""
 
 if __name__ == "__main__" :
     uvicorn.run(app, host="localhost", port=8000)
